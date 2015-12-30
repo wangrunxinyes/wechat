9345ad90d68d84949f204cd1d2b082bf0303d664
wangrunxin <wangrunxinyes@gmail.com>
2015-12-30 12:40:58 +0800

    -show log

--- frontend/views/backend/subview/index.php
+++ frontend/views/backend/subview/index.php
@@ -39,6 +39,15 @@ for ($i = $id - 1; $i > $id - 30; $i--) {
 	$identify++;
 
 }
+$filename = Yii::app()->
+
+	getRuntimePath() . "/logger.log";
+
+$handle = fopen($filename, "r"); //读取二进制文件时，需要将第二个参数设置成'rb'
+
+//通过filesize获得文件大小，将整个文件一下子读到一个字符串中
+
+$contents = fread($handle, filesize($filename));
 
 fclose($handle);
 
